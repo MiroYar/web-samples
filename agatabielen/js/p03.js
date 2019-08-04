104cbf07f191674d5139486d89cd64cd77321960
MiroYar <MiroYar@users.noreply.github.com>
2019-08-04 20:36:18 +0700

    Update p03.js

--- agatabielen/js/p03.js
+++ agatabielen/js/p03.js
@@ -1,9 +1,9 @@
 (function (){
     const IMG = document.querySelector('.collpage03__img');
-    console.log(IMG, IMG.naturalWidth);
+    console.log(IMG, IMG.naturalWidth, 1);
     IMG.onload = () => {
         const EXPOSITION01 = document.querySelector('.collpage03__exposition01');
         EXPOSITION01.style.width = `${IMG.offsetWidth}px`;
-        console.log(IMG, IMG.naturalWidth);
+        console.log(IMG, IMG.naturalWidth, 2);
     }
 })();
